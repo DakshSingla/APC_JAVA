d708743f651d91c95d743b5da378c48d62493f8d
DakshSingla <dakshsingla465@gmail.com>
2025-07-15 16:42:43 +0530

    its 15 july 2025 its apc 2 time

--- I/O/test.java
+++ I/O/test.java
@@ -4,6 +4,7 @@ public class test {
     public static void main(String[] args) {
         File file = new File("example.txt");
         System.out.println("File exists : "+ file.exists()); // tels that the fiel is exists otr not
-
+        System.out.println("is file  : " + file.isFile()); // tell that the fiel is fiewl 
+        System.out.println("is file  : " + file.isDirectory());
     }
 }
