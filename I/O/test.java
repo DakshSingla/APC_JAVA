f9b2406c8b50ea53fc056a0990477a35a38ed149
DakshSingla <dakshsingla465@gmail.com>
2025-07-15 16:40:09 +0530

    its 15 july 2025 its apc

---
+++ I/O/test.java
@@ -0,0 +1,9 @@
+import java.io.File;
+
+public class test {
+    public static void main(String[] args) {
+        File file = new File("example.txt");
+        System.out.println("File exists : "+ file.exists()); // tels that the fiel is exists otr not
+
+    }
+}
