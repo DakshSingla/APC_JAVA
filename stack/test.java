7c917bbb167513dbb9c2009546e70caf649bdde0
DakshSingla <dakshsingla465@gmail.com>
2025-07-16 17:14:49 +0530

    it is practice

--- stack/test.java
+++ stack/test.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        Stack <String>  st = new Stack<>();
+        Deque <String>  st = new ArrayDeque<>();
         st.push("A");
          st.push("B");
           st.push("C");
-          System.out.println("TOp:" + st.peek());
+          System.out.println("Top:" + st.peek());
           while (!st.isEmpty()) {
             System.out.println("Popped: "+ st.pop());
           }
